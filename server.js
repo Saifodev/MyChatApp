af4a7c4a05d5ffaf7eb9a917e4ecd4a1fa93b40a
Saifodev <113042148+Saifodev@users.noreply.github.com>
2022-10-08 13:47:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-
+const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -18,7 +18,7 @@ io.on('connection', (socket) => {
         console.log('user disconnected');
       });
   });
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log('listening on *:3000');
 });
 
